cec36f1a4993cebc724d2520fc452fd554669104
Suryanshu Patnaik <er.suryanshupatnaik.4@gmail.com>
2023-01-08 13:19:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function alert_1(){
+    alert("Verification of Superposition Theorem using two independent voltage sources")
+}
+function alert_2(){
+    alert("Verification of Superposition Theorem using one independent voltage source and one independent current source")
+}
+function alert_3(){
+    alert("Verification of Superposition Theorem using two independent current sources")
+}
